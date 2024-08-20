20941eaa111151856db8b8d4e6b958825c9c9bd2
Leaderson <108360654+Leadhason@users.noreply.github.com>
2024-08-20 17:46:13 +0000

    favicon

--- a_portfolio/app/layout.js
+++ a_portfolio/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Mr. White",
+  title: "Leadhason",
   description: "This is a sleek, dark-themed portfolio website.",
 };
 
